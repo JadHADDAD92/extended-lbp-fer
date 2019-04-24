e0e155bfe4dbe2d1b2426b1d49e18426404f69fe
Jad Haddad <jad.haddad@zto-technology.com>
2019-04-24 16:19:19 +0200

    Add approximate time remaining in gridsearch

--- python/gridsearch.py
+++ python/gridsearch.py
@@ -2,6 +2,7 @@
 
 import argparse
 from decimal import Decimal as D
+from datetime import timedelta
 from pathlib import Path
 from time import time
 import csv
@@ -48,14 +49,17 @@ def learn(filePath):
 
     models = []
     models.append(['gamma \ C', 1.00E-12, 1.00E-11, 1.00E-10, 1.00E-09, 1.00E-08,
-                1.00E-07, 1.00E-06, 1.00E-05, 1.00E-04, 1.00E-03, 2.00E-03, 1.00E-02,
-                1.00E-01, 1.00, 1.00E+01, 1.00E+02, 1.00E+03, 1.00E+04, 1.00E+05 ])
-    a = time()
+                   1.00E-07, 1.00E-06, 1.00E-05, 1.00E-04, 1.00E-03, 2.00E-03, 1.00E-02,
+                   1.00E-01, 1.00, 1.00E+01, 1.00E+02, 1.00E+03, 1.00E+04, 1.00E+05 ])
+    gridTimeStart = time()
+    numIteration = len(parameters['gamma']) * len(parameters['C'])
+    iteration = 0
+    meanTime = 0
     for gamma in parameters['gamma']:
         row = [gamma]
         for C in parameters['C']:
-            b = time()
             print('C = %s \t gamma = %s'%(C, gamma))
+            timeStart = time()
             svc = OneVsRestClassifier(SVC(random_state=0, decision_function_shape='ovr',
                                         C=C, kernel='rbf', gamma=gamma), n_jobs=4)
             svc.fit(xTrain, yTrain)
@@ -67,10 +71,17 @@ def learn(filePath):
             print("accuracy: %d/%d = "%(correct, len(yTrue)),
                 D('%.2f'%(correct/len(yTrue)*100)))
             row.append(D('%.2f'%(correct/len(yTrue)*100)))
-            print('--------------------------(%d sec)'%(time()-b))
+            
+            iterTime = time()-timeStart
+            iteration = iteration + 1
+            meanTime = meanTime * (iteration-1)/iteration + iterTime/iteration
+            remainingTime = (numIteration-iteration)*meanTime
+            print('--------------------------(%d sec)--remaining: %s'%
+                  (iterTime, str(timedelta(seconds=int(remainingTime)))))
         models.append(row)
-
-    print('time: ', D('%.2f'%(time() - a)), "sec")
+    gridTime = time() - gridTimeStart
+    gridTime = timedelta(seconds=int(gridTime))
+    print('time: %s'%str(gridTime))
     print('saving file: %s.csv'%filename)
     with open('%s.csv'%filename, 'w') as csvFile:
         writer = csv.writer(csvFile)
